6caba88fef5271c8199784716e34574470d38b9c
William Wright <162603673+wwrigh18@users.noreply.github.com>
2025-10-03 03:19:13 +0000

    After a lot of trial and error, defined test_sample_run_anonymizer ina way that I THINK is supposed to be fine

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -2,5 +2,7 @@ import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
-    # replace the following line with your test
+    
+    assert sample_run_anonymizer('My name is Bond.', 'PERSON', 0.8, 11, 15).text == "My name is BIP."
+    
     pass
